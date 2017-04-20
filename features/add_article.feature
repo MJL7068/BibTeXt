78cef81c821272a864feb9bd47b71eb7583ae731
impliedfeline <kinnunen.jussi@hotmail.com>
2017-04-20 23:30:43 +0300

    Test article adding feature with cucumber

---
+++ features/add_article.feature
@@ -0,0 +1,13 @@
+Feature: As a user, I want to add new article references
+
+	Scenario: user can add new valid article reference
+		Given create page is selected
+		And create new article reference is selected
+		When reference key "X", author "Jussi", journal "Mestarit", title "Paras", year "2017" are given
+		Then new article reference with field "Jussi" is created
+
+	Scenario: user can not add invalid article reference
+		Given create page is selected
+		And create new article reference is selected
+		When no fields are given
+		Then new article reference is not created and error message is given
---
+++ features/step_definitions/add_article_steps.rb
@@ -0,0 +1,31 @@
+Given(/^create page is selected$/) do
+  visit "/"
+  click_on "Create Page"
+end
+
+Given(/^create new article reference is selected$/) do
+  click_on "Create new article reference"
+end
+
+When(/^no fields are given$/) do
+  click_on "Create Article"
+end
+
+When(/^reference key "(.*)", author "(.*)", journal "(.*)", title "(.*)", year "(.*)" are given$/) do
+  |ref_key, author, journal, title, year|
+  fill_in "Ref key", with: ref_key
+  fill_in "Author", with: author
+  fill_in "Journal", with: journal
+  fill_in "Title", with: title
+  fill_in "Year", with: year
+  click_on "Create Article"
+end
+
+Then(/^new article reference with field "(.*)" is created$/) do |field|
+  expect(page).to have_content "Article was successfully created"
+  expect(page).to have_content field
+end
+
+Then(/^new article reference is not created and error message is given$/) do
+  expect(page).to have_content "error"
+end
