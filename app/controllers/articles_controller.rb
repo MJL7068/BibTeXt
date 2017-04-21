de000a67268917e287822bd464c9d37dd5933210
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-04-21 15:27:16 +0300

    Implement randomized hash (#28)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,5 +69,6 @@ class ArticlesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:ref_key, :author, :journal, :title, :volume, :number, :pages, :year, :month, :note, :page_id)
-    end
+   end
+
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ApplicationRecord
+    before_create :randomize_id
     has_many :articles
 
 	def get_articles
@@ -7,14 +8,21 @@ class Page < ApplicationRecord
 
     def to_bibtex
         output = ""
-        
+
         # TODO: when we implement other reference types, create more loops here
         # (or combine them to single array, since all of them should have .to_bibtext method)
         get_articles.each do |a|
             output += a.to_bibtex
             output += "\n\n"
         end
-        
+
         return output
     end
+
+    private
+    def randomize_id
+     begin
+       self.id = rand(1000000...10000000)
+     end while Page.where(id: self.id).exists?
+   end
 end
