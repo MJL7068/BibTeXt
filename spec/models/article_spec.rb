bfc8acf5ad8f2d10d149e439331bb73e6520dd90
Antti Lipsanen <antti.lip@gmail.com>
2017-04-28 00:56:57 +0300

    Add tests for ref_key generation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,10 +7,23 @@ RSpec.describe Article, type: :model do
       expect(article).to be_valid
     end
   end
+
   describe "with no fields" do
     it "is not saved" do
       article = Article.new
       expect(article).not_to be_valid
+    end
+  end
+
+  describe "saved without ref_key" do
+    it "is saved" do
+      article = FactoryGirl.create(:article, ref_key: '')
+      expect(article).to be_valid
+    end
+
+    it "has non-zero length ref_key" do
+      article = FactoryGirl.create(:article, ref_key: '')
+      expect(article.ref_key.length).to be > 0
     end
   end
 end
--- spec/models/in_proceeding_spec.rb
+++ spec/models/in_proceeding_spec.rb
@@ -7,10 +7,23 @@ RSpec.describe InProceeding, type: :model do
       expect(in_proceeding).to be_valid
     end
   end
+
   describe "with no fields" do
     it "is not saved" do
       in_proceeding = InProceeding.new
       expect(in_proceeding).not_to be_valid
+    end
+  end
+
+  describe "saved without ref_key" do
+    it "is saved" do
+      in_proceeding = FactoryGirl.create(:in_proceeding, ref_key: '')
+      expect(in_proceeding).to be_valid
+    end
+
+    it "has non-zero length ref_key" do
+      in_proceeding = FactoryGirl.create(:in_proceeding, ref_key: '')
+      expect(in_proceeding.ref_key.length).to be > 0
     end
   end
 end
